f29d95fac22c7424febea7ac520a61fce2b044ca
Jaime <nguyenphuocdat168@gmail.com>
2025-01-09 01:28:06 -0500

    type safety with sanity typegen

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, {StartupCardType} from "@/components/StartupCard";
+import {client} from "@/sanity/lib/client";
+import {STARTUP_QUERY} from "@/sanity/lib/query";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
@@ -7,16 +9,7 @@ export default async function Home({ searchParams }: {
 
   const query = (await searchParams).query
 
-  const posts = [{
-    createdAt: new Date(),
-    views: 55,
-    author: { _id: 1, name: 'Jaime' },
-    _id: 1,
-    description: "This is a long description",
-    image: "https://plus.unsplash.com/premium_photo-1666901328734-3c6eb9b6b979?q=80&w=2380&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "Animated",
-    title: "Tree Lightning",
-  }]
+  const posts = await client.fetch(STARTUP_QUERY)
 
   return (
     <>
@@ -35,7 +28,7 @@ export default async function Home({ searchParams }: {
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupCardType) => (
               <StartupCard key={post?._id} post={post}/>
             ))
           ): (
